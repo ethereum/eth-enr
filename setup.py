9059156c87f1a8053186e558b0d97f6d2f074a48
Piper Merriam <pipermerriam@gmail.com>
2020-09-02 09:12:41 -0600

    Bump version: 0.1.0-alpha.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ with open('./README.md') as readme:
 setup(
     name='eth-enr',
     # *IMPORTANT*: Don't manually change the version here. Use `make bump`, as described in readme
-    version='0.1.0-alpha.0',
+    version='0.2.0',
     description="""eth-enr: Python library for ENR (EIP-778) records""",
     long_description=long_description,
     long_description_content_type='text/markdown',
